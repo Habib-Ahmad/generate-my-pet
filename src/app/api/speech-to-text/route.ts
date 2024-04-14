b3163b0565b157b39bc909b412d76d3eba62444c
Ahmad  Habib <TAJ531@tajbank.com>
2024-04-14 02:53:15 +0100

    Added edge runtime to apis

--- src/app/api/speech-to-text/route.ts
+++ src/app/api/speech-to-text/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
+export const runtime = "edge";
+
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const { base64Data } = body;
